5e98136ccb88ef4aefefb3529cc61c591336b591
Adam Griswold <adam.griswold@nucor.com>
2023-11-18 23:20:46 +0000

    🚨 (docs): fix linting errors

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,29 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-function Gradient({
-  conic,
-  className,
-  small,
-}: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
-}): JSX.Element {
-  return (
-    <span
-      className={[
-        styles.gradient,
-        conic ? styles.glowConic : undefined,
-        small ? styles.gradientSmall : styles.gradientLarge,
-        className,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    />
-  );
-}
-
 export default function Page(): JSX.Element {
   return (
     <main className={styles.main} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
