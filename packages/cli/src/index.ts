86a512cab16bacddae742d102226a082b954112e
Adam Griswold <adam.griswold@nucor.com>
2023-11-20 14:21:54 +0000

    ✨ (cli): Grab live package version from package.json

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,15 +4,19 @@ import { Command } from "commander"
 
 import { init } from "@/src/command/init"
 
+import { getPackageInformation } from '@/src/utils/get-package-information'
+
 process.on("SIGINT", () => process.exit(0))
 process.on("SIGTERM", () => process.exit(0))
 
 async function main() {
+    const packageInformation = getPackageInformation()
+
     const cli = new Command()
         .name("aoc-cli")
         .description("add advent of code files and data to your project")
         .version(
-            "0.0.5",
+            packageInformation.version ?? "0.0.5",
             "-v, --version",
             "display the version number"
         )
