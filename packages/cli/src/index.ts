4138197ce636e92ccbde706261f8a3c6ac5bd0a9
Adam Griswold <adam.griswold@nucor.com>
2023-11-20 14:02:26 +0000

    ✨ (cli): End process on SIGINT and SIGTERM

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,6 +4,9 @@ import { Command } from "commander"
 
 import { init } from "@/src/command/init"
 
+process.on("SIGINT", () => process.exit(0))
+process.on("SIGTERM", () => process.exit(0))
+
 async function main() {
     const cli = new Command()
         .name("aoc-cli")
